3bab6912e3f8b994bdb3a69a28cc697376ba9e48
Marcys06 <marcelcieslik6@gmail.com>
2026-08-26 19:49:32 +0200

    feat: initialize RailDispatch project structure

---
+++ src/RailDispatch.Domain/Map/GameMap.cs
@@ -0,0 +1,42 @@
+﻿namespace RailDispatch.Domain.Map;
+
+public sealed class GameMap
+{
+    private readonly MapCell[] _cells;
+
+    public MapSize Size { get; }
+
+    public GameMap(int width, int height)
+        : this(new MapSize(width, height))
+    {
+    }
+
+    public GameMap(MapSize size)
+    {
+        Size = size;
+        _cells = new MapCell[size.Width * size.Height];
+
+        for (var i = 0; i < _cells.Length; i++)
+            _cells[i] = new MapCell();
+    }
+
+    public MapCell GetCell(MapPosition position)
+    {
+        ValidatePosition(position);
+        return _cells[position.Y * Size.Width + position.X];
+    }
+
+    public void SetTerrain(MapPosition position, TerrainType terrain)
+    {
+        GetCell(position).Terrain = terrain;
+    }
+
+    private void ValidatePosition(MapPosition position)
+    {
+        if (position.X < 0 || position.X >= Size.Width)
+            throw new ArgumentOutOfRangeException(nameof(position));
+
+        if (position.Y < 0 || position.Y >= Size.Height)
+            throw new ArgumentOutOfRangeException(nameof(position));
+    }
+}
---
+++ src/RailDispatch.Domain/Map/TerrainType.cs
@@ -0,0 +1,9 @@
+﻿namespace RailDispatch.Domain.Map;
+
+public enum TerrainType
+{
+    Grass,
+    Forest,
+    Hill,
+    Mountain
+}
